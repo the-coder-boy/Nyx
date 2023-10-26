d55dd3f2bc1f4d389f8dadadc9a678e25d022703
the-coder-boy <shreejanOfficial2007@gmail.com>
2023-10-26 11:39:28 +0530

    index.html

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
-def index(requests):
-    return HttpResponse("hello")
+def index(request):
+    return render(request,'index.html')
